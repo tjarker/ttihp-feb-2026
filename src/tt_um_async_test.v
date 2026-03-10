7c3048c85486f485e7eb054ed2239b1842ede7e3
Tjark Petersen <tjark-petersen@gmx.de>
2026-03-10 17:46:01 +0100

    add um prefix

--- src/tt_um_async_test.v
+++ src/tt_um_async_test.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_async_test (
+module tt_um_async_test (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
